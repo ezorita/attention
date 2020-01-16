944a730a79951ba17b4ca7eea3443f91b2822a4b
Eduard Valera i Zorita <eduardvalera@gmail.com>
2020-01-16 11:24:08 +0100

    Clean 'protein_albert' file

--- protein_albert.py
+++ protein_albert.py
@@ -1,9 +1,4 @@
 import sys
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.multiprocessing as mp
-import numpy as np
 import matplotlib.pyplot as plt
 
 # Local modules
@@ -11,7 +6,7 @@ import sequence
 import attention
 from lookahead import Lookahead
 
-# Avoid plot crash without X-server
+# Avoid plot crash when no X-server is available
 plt.switch_backend('agg')
 
 
@@ -31,7 +26,7 @@ if __name__ == "__main__":
       )
 
       epochs = 300
-      batch_size = 16
+      batch_size = 64
       lr = 0.0001
       train_sop_loss, train_mlm_loss, test_sop_loss, test_mlm_loss = albert.pretrain(seqs, epochs=epochs, batch_size=batch_size, lr=lr)
       torch.save(albert.state_dict(), 'albert_model_statedict.torch')
